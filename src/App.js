99d1b78615a56550c98a8c454eeac8ec2714df94
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-10 00:30:26 -0300

    Add the Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './assets/css/App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
-    <div className="App">
-      Costs
+    <div className="container">
+      <Router>
+        <ul>
+          <li><Link to='/'>Home</Link> </li>
+          <li><Link to='/projects'>Projetos</Link> </li>
+          <li><Link to='/company'>Companhia</Link> </li>
+          <li><Link to='/contact'>Contatos</Link> </li>
+        </ul>
+        {/* Will be replaced for a component Header */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newProject' element={<NewProject />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/company' element={<Company />} />
+        </Routes>
+        <footer>footer</footer> {/* Will be replaced for a component Footer */}
+      </  Router>
+
     </div>
   );
 }
