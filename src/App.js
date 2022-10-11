693d093fcd014aa0cd107cdc28d35a4a59df2b7d
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-10 22:08:29 -0300

    add component layout
    - removed import Link of react-router
    - Add component Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './assets/css/App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects'
 import Container from './components/layout/Container/index';
+import Header from './components/layout/Header/index';
+import Footer from './components/layout/Footer/index';
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/projects'>Projetos</Link> 
-        <Link to='/company'>Companhia</Link> 
-        <Link to='/contact'>Contatos</Link> 
-      </div>
-      {/* Will be replaced for a component Header */}
+      <Header/>
       <Container customClass='grow'>
         <Routes>
           <Route path='/' element={<Home />} />
@@ -27,7 +23,7 @@ function App() {
           <Route path='/company' element={<Company />} />
         </Routes>
       </Container>
-      <footer>footer</footer> {/* Will be replaced for a component Footer */}
+      <Footer/>
     </Router>
   );
 }
