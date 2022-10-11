3ebebcd905fce0af95f8070cdac1475c57f86807
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-10 22:05:55 -0300

    switch name tag

--- src/components/layout/Container/index.js
+++ src/components/layout/Container/index.js
@@ -2,7 +2,7 @@ import styles from './style.module.css'
 
 const Container = (props) => {
   return (
-    <main className={`${styles.container} ${styles[props.customClass]}`} >{props.children}</main>
+    <div className={`${styles.container} ${styles[props.customClass]}`} >{props.children}</div>
   )
 }
 
