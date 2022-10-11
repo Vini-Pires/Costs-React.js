f7ddee62b5a4c6f9e16d3626f46417475cd51aa0
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-11 12:00:34 -0300

    creation of variables for css
    - imported on *__App.js__* the *__root.css__*
    - create *__root.css__* with variables for colors and font family
    - deleted the *__App.css__*

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './assets/css/App.css';
+import './assets/css/root.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
