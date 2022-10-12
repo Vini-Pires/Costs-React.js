54943ab709d65633361a26a2f9c5ffb3b909b430
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-12 14:01:02 -0300

    NewProject - page and style

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import ProjectForm from '../project/ProjectForm/index'
+import style from  '../../assets/css/NewProject.module.css'
 
 const NewProject = () => {
   return (
-    <div>NewProject</div>
+    <section className={style.new_project}>
+      <div className={style.container}>
+        <h1>Criar Projeto</h1>
+        <p>Crie seu projeto para depois adicionar os serviços</p>
+        <ProjectForm btnText='Criar Projeto'/>
+      </div>
+    </section>
   )
 }
 
