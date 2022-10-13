e1269f94a2cf7a80d0e9fe31c7ef40e91ee1ce84
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-13 15:31:22 -0300

    Select - update component
    transformed the component to be adaptable, based on database info

--- src/components/layout/Form/Select/index.js
+++ src/components/layout/Form/Select/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import style from './style.module.css'
 
-const Select = ({placeholder, name, options, handleOnChange, text}) => {
+const Select = ({name, options, text, value, handleOnChange}) => {
   return (
     <div className={style.form_control}>
       <label htmlFor={name}>{text}</label>
-      <select name={name} id={name}>
-        <option defaultChecked>Selecione uma categoria</option>
-        <option value="">tecnologia</option>
+      <select name={name} id={name} onChange={handleOnChange} defaultValue={value || ''} >
+        <option defaultValue={text}>{text}</option>
+        {options.map((option)=>(
+          <option value={option.id} key={option.id}>{option.name}</option>
+        ))}
+        
       </select>
     </div>
   )
