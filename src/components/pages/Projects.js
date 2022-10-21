6f05788b51e2855c20d2c091f50becf8ade9fc72
Vini Pires <viniciusazevedopires@gmail.com>
2022-10-21 14:37:09 -0300

    Prettier and useState on message

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,53 +1,55 @@
-import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import Message from 'components/layout/Message'
-import Container from 'components/layout/Container'
-import LinkButton from 'components/layout/LinkButton'
-import ProjectCard from 'components/project/ProjectCard'
-import style from  'assets/css/Projects.module.css'
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import Message from "components/layout/Message"
+import Container from "components/layout/Container"
+import LinkButton from "components/layout/LinkButton"
+import ProjectCard from "components/project/ProjectCard"
+import style from "assets/css/Projects.module.css"
 
-const Projects = ()  => {
-  
-  let message = ''
-  const [projects, setProjects] = useState([]);
+const Projects = () => {
+  const [message, setMessage] = useState("")
+  const [projects, setProjects] = useState([])
   const location = useLocation()
   if (location.state) {
-    message = location.state.message
+    setMessage(location.state.message)
   }
 
   useEffect(() => {
-    let url = 'http://localhost:1212/projects'
+    let url = "http://localhost:1212/projects"
     fetch(url, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type' : 'application/json',
+        "Content-Type": "application/json",
       },
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setProjects(data)
-    })
-    .catch(err => console.log(err))
-
-  }, []);
+      .then(res => res.json())
+      .then(data => {
+        setProjects(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
   return (
     <section className={style.projects}>
       <div className={style.projects_title}>
         <h1>Meus Projetos</h1>
-        <LinkButton toWhere='/newProject' text='Criar Projeto'/>
+        <LinkButton toWhere='/newProject' text='Criar Projeto' />
       </div>
-      {message && <Message message={message} type='success'/>}
+      {message && <Message message={message} type='success' />}
       <Container customClass='start'>
         {projects.length > 0 &&
           projects.map(e => (
-                <ProjectCard name={e.name} id={e.id} budget={e.budget} category={e.category.name} handleRemove='none' key={e.id} />
-            )
-          )
-        }
+            <ProjectCard
+              name={e.name}
+              id={e.id}
+              budget={e.budget}
+              category={e.category.name}
+              handleRemove='none'
+              key={e.id}
+            />
+          ))}
       </Container>
     </section>
-    )
+  )
 }
 
 export default Projects
